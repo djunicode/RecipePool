1dad9ec57207e923ca11200baf3809cf94a24b86
AnaidaLewis <anaidallewis@gmail.com>
2022-04-03 11:27:24 +0530

    SECRET_KEY env

--- backend/RecipePool/RecipePool/settings.py
+++ backend/RecipePool/RecipePool/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nb&ou-j)z&v4_e$sk19j)o0^57168a728lpyp*4#d7hw2*keae'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
