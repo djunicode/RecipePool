df050b359ccf7be40cbe522d4b669f0c5b030f44
Abhijit Saha <asoksha715@gmail.com>
2022-04-10 21:37:25 +0530

    Food Card Recycler View Added

---
+++ android/App/app/src/main/java/com/example/recipepool/FoodList.kt
@@ -0,0 +1,3 @@
+package com.example.recipepool
+
+class FoodList(val name: String, val time: String, val favourite: Boolean, var rating: Float)
--- android/App/app/src/main/java/com/example/recipepool/MainActivity.kt
+++ android/App/app/src/main/java/com/example/recipepool/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.recipepool
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.recipepool.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
