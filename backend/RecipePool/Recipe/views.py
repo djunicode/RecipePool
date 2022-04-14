68d6fdda4347060c7a4f7e82f681cc11a130bec0
Jenil-Savla <you@example.com>
2022-04-14 09:27:09 +0530

    Created API to query databse using OR condition.

--- backend/RecipePool/Recipe/views.py
+++ backend/RecipePool/Recipe/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from .models import Ingredient, IngredientList, Recipe
+from .serializers import RecipeSerializer
 
-# Create your views here.
+#Query the database for Recipe using given ingredients with OR condition 
+class RecipeSearchOR(generics.ListAPIView):
+    serializer_class = RecipeSerializer
+
+    def get_queryset(self):
+        ingredient_query = self.request.query_params.get('q',None).lower().split('|') #Extract the query string and insert the ingredients in a query list
+        ingredient = Ingredient.objects.filter(name__in = ingredient_query)  #Filter ingredients present in query list
+        ingredient_list = IngredientList.objects.filter(ingredient__in = ingredient) #Filter Ingredient Lists with the given query ingredients 
+        recipe = Recipe.objects.filter(id__in = ingredient_list) #List the recipes related to the ingredients in the ingredient list
+        return recipe
