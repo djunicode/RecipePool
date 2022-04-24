beb4c90a9e753e99a128ecf6acd561b5e6bc2d61
Yash <yashr10102@gmail.com>
2022-04-24 17:52:52 +0530

    Login and SignUp details

--- android/App/app/src/main/java/com/example/recipepool/SignUp.kt
+++ android/App/app/src/main/java/com/example/recipepool/SignUp.kt
@@ -1,6 +1,7 @@
 package com.example.recipepool
 
 import android.app.DatePickerDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
@@ -37,6 +38,71 @@ class SignUp : AppCompatActivity() {
                 cal.get(Calendar.MONTH),
                 cal.get(Calendar.DAY_OF_MONTH)
             ).show()
+        }
+
+        binding.btSignup.setOnClickListener {
+
+            // if NAME is empty
+            if (binding.etName.text.toString().trim().isEmpty()){
+
+                binding.etName.error = "Name Required"
+                binding.etName.requestFocus()
+                return@setOnClickListener
+            }
+
+            //if PASSWORD is empty
+            val password = binding.etPassword.text.toString().trim()
+            if (password.isEmpty() || password.length<6){
+                binding.etPassword.error = "Minimum 6 characters required"
+                binding.etPassword.requestFocus()
+                return@setOnClickListener
+            }
+
+            //if PASSWORDS don't match
+            val confirm_password = binding.etConfirmpass.text.toString().trim()
+
+            if (password != password){
+                binding.etConfirmpass.error = "Passwords don,t match"
+                binding.etConfirmpass.requestFocus()
+                return@setOnClickListener
+            }
+
+            // checking email
+            val email = binding.etEmail.text.toString().trim()
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+
+                binding.etEmail.error = "Invalid email"
+                binding.etEmail.requestFocus()
+                return@setOnClickListener
+
+            }
+
+            //checking DOB
+            if (binding.etDate.text.isEmpty()){
+
+                binding.etDate.error = "Date of Birth required"
+                binding.etDate.requestFocus()
+                return@setOnClickListener
+            }
+
+            //checking phone number
+            val number = binding.etPhone.text.toString().trim()
+            if (number.isEmpty() || number.length!=10){
+                binding.etPhone.error = "Invalid phone number"
+                binding.etPhone.requestFocus()
+                return@setOnClickListener
+            }
+
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+
+        }
+
+        binding.tvLogin.setOnClickListener {
+
+            val intent = Intent(this,Login::class.java)
+            startActivity(intent)
         }
 
 
